896107128810c9030d97b37f13b3b0792213c2a9
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-04-20 02:26:39 +0100

    Post actions and helpers not needed yet

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,84 +1,11 @@
-import 'babel-polyfill';
-import _ from 'lodash';
-import { FETCH_POSTS, REMOVE_POST, FETCH_POSTS_ERROR, FETCH_MEDIA_END, FETCH_MEDIA_ITEM, FETCH_MEDIA_ITEM_ERROR, FETCH_MEDIA_ITEM_START } from './types';
-import { getDiscussionsFromAPI, getReviewTypeFromPost, getSeasonNumberFromPost, getEpisodeNumberFromPost, getMediaItem, getTmdbIdFromPost } from '../helpers/apiHelpers';
+import { getDiscussionsFromAPI } from '../helpers/apiHelpers';
 
-const fetchMediaItemsFromPosts = posts => dispatch => Promise.all(posts.map(post =>
-  getMediaItem(getReviewTypeFromPost(post), {
-    id: getTmdbIdFromPost(post),
-    season_number: getSeasonNumberFromPost(post),
-    episode_number: getEpisodeNumberFromPost(post),
-  })
-    .then((mediaItem) => {
-      const item = mediaItem;
-      item.mediaType = getReviewTypeFromPost(post);
-      dispatch({
-        type: FETCH_MEDIA_ITEM,
-        payload: item,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: FETCH_MEDIA_ITEM_ERROR,
-        payload: err,
-      });
-      dispatch({
-        type: REMOVE_POST,
-        payload: post,
-      });
-    })));
-
-export const fetchPosts = ({ sortBy = 'trending', category, limit = 10 }) => (dispatch) => {
-  getDiscussionsFromAPI(sortBy, { category, limit })
-    .then((posts) => {
-      dispatch({
-        type: FETCH_POSTS,
-        payload: posts,
-      });
-      dispatch(fetchMediaItemsFromPosts(posts)).then(() => dispatch({
-        type: FETCH_MEDIA_END,
-      }));
-    })
-    .catch(err => dispatch({
-      type: FETCH_POSTS_ERROR,
-      payload: err,
-    }));
-};
-
-export const fetchMediaItem = (id, reviewType, seasonNum, episodeNum) => (dispatch) => {
-  getMediaItem(reviewType, { id, episode_number: episodeNum, season_number: seasonNum })
-    .then((mediaItem) => {
-      const item = mediaItem;
-      item.mediaType = reviewType;
-      if (reviewType === 'episode' && !_.isEmpty(item[`season/${seasonNum}/episode/${episodeNum}`])) {
-        item.episodeNum = episodeNum;
-        item.seasonNum = seasonNum;
-        dispatch({
-          type: FETCH_MEDIA_ITEM,
-          payload: item,
-        });
-      } else if (reviewType !== 'episode') {
-        dispatch({
-          type: FETCH_MEDIA_ITEM,
-          payload: item,
-        });
-      } else {
-        dispatch({
-          type: FETCH_MEDIA_ITEM_ERROR,
-          payload: { message: 'Season or episode not found' },
-        });
-      }
-    })
-    .catch(err => dispatch({
-      type: FETCH_MEDIA_ITEM_ERROR,
-      payload: err,
-    }));
-};
-
-export const fetchMediaItemStart = () => (dispatch) => {
-  dispatch({
-    type: FETCH_MEDIA_ITEM_START,
-  });
-};
+export const fetchPosts = ({ sortBy = 'created', category, limit = 10 }) => ({
+  type: 'FETCH_POSTS',
+  payload: getDiscussionsFromAPI(sortBy, { category, limit }),
+  meta: {
+    globalError: 'Sorry, there was an error fetching the reviews',
+  },
+});
 
 export default fetchPosts;
--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -1,124 +1,56 @@
 import theMovieDBAPI from '../apis/theMovieDBAPI';
-import steemAPI from '../apis/steemAPI';
-
-export const getDiscussionsFromAPI = (sortBy, query) =>
-  new Promise((resolve, reject) => {
-    switch (sortBy) {
-      case 'trending':
-        return new Promise(() => {
-          steemAPI.getDiscussionsByTrending(query, (err, result) => {
-            const posts = [];
-            if (result) {
-              result.forEach((post) => {
-                if (JSON.parse(post.json_metadata).tags.find(tag => tag.includes('-review'))) {
-                  posts.push(post);
-                }
-              });
-            }
-            if (err !== null) return reject(Error(err));
-            return resolve(posts);
-          });
-        });
-      case 'created':
-        return new Promise(() => {
-          steemAPI.getDiscussionsByCreated(query, (err, result) => {
-            const posts = [];
-            if (result) {
-              result.forEach((post) => {
-                if (JSON.parse(post.json_metadata).tags.find(tag => tag.includes('-review'))) {
-                  posts.push(post);
-                }
-              });
-            }
-            if (err !== null) return reject(Error(err));
-            return resolve(posts);
-          });
-        });
-      case 'hot':
-        return new Promise(() => {
-          steemAPI.getDiscussionsByHot(query, (err, result) => {
-            const posts = [];
-            if (result) {
-              result.forEach((post) => {
-                if (JSON.parse(post.json_metadata).tags.find(tag => tag.includes('-review'))) {
-                  posts.push(post);
-                }
-              });
-            }
-            if (err !== null) return reject(Error(err));
-            return resolve(posts);
-          });
-        });
-      case 'promoted':
-        return new Promise(() => {
-          steemAPI.getDiscussionsByPromoted(query, (err, result) => {
-            const posts = [];
-            if (result) {
-              result.forEach((post) => {
-                if (JSON.parse(post.json_metadata).tags.find(tag => tag.includes('-review'))) {
-                  posts.push(post);
-                }
-              });
-            }
-            if (err !== null) return reject(Error(err));
-            return resolve(posts);
-          });
-        });
-      default:
-        return reject(Error('There is no API endpoint defined for this sorting'));
-    }
-  });
-
-export const getDiscussionsByCreated = query =>
-  new Promise((resolve, reject) => {
-    steemAPI.getDiscussionsByCreated(query, (err, result) => {
-      const posts = [];
-      result.forEach((post) => {
-        if (JSON.parse(post.json_metadata).tags.find(tag => tag.includes('-review'))) {
-          posts.push(post);
-        }
-      });
-      if (err !== null) return reject(Error(err));
-      return resolve(posts);
-    });
-  });
 
 export const getMediaItem = (reviewType, query) =>
   new Promise((resolve, reject) => {
     switch (reviewType) {
       case 'movie':
-        return new Promise(() =>
-          theMovieDBAPI.movieInfo(query.id)
-            .then(movie => resolve(movie))
-            .catch(err => reject(err)));
+        return resolve(theMovieDBAPI.movieInfo(query.id));
       case 'show':
         return resolve(theMovieDBAPI.tvInfo(query.id));
-      case 'episode':
-        return resolve(theMovieDBAPI.tvInfo({
+      case 'episode': {
+        if (query.season_number && query.episode_number) {
+          return new Promise(() => theMovieDBAPI.tvInfo({
+            id: query.id,
+            append_to_response: `season/${query.season_number}/episode/${query.episode_number}`,
+          }).then((response) => {
+            if (response[`season/${query.season_number}/episode/${query.episode_number}`]) {
+              return resolve(response);
+            }
+            return reject(Error('Episode or season not found'));
+          }));
+        }
+        return reject(Error('No episode or season number defined'));
+      }
+      case 'season': {
+        if (query.seasonNum) {
+          return resolve(theMovieDBAPI.tvSeasonInfo({
+            id: query.id,
+            season_number: query.seasonNum,
+          }));
+        }
+        return reject(Error('No season number defined'));
+      }
+      case 'seasonshow': {
+        return new Promise(() => theMovieDBAPI.tvInfo({
           id: query.id,
-          append_to_response: `season/${query.season_number}/episode/${query.episode_number}`,
+          append_to_response: `season/${query.seasonNum}`,
+        }).then((response) => {
+          if (response[`season/${query.seasonNum}`]) {
+            return resolve(response);
+          }
+          return reject(Error('Show or season not found'));
         }));
+      }
+      case 'search': {
+        if (query.q) {
+          return resolve(theMovieDBAPI.searchMulti({ query: query.q }));
+        }
+        return resolve({ results: [] });
+      }
+
       default:
         return reject(Error('There is no API endpoint defined for this review type'));
     }
   });
 
-export const getReviewTypeFromPost = post => JSON.parse(post.json_metadata).tags.find(tag => tag.includes('-review')).replace('-review', '');
-export const getTmdbIdFromPost = post => JSON.parse(post.json_metadata).tags.find(tag => tag.includes('tmdbid-')).replace('tmdbid-', '');
-export const getEpisodeNumberFromPost = post => JSON.parse(post.json_metadata).episode_num;
-export const getSeasonNumberFromPost = post => JSON.parse(post.json_metadata).season_num;
-
-export function getMediaItems(posts) {
-  return Promise.all(posts.map((post) => {
-    const itemPromise = getMediaItem(getReviewTypeFromPost(post), {
-      id: getTmdbIdFromPost(post),
-      season_number: getSeasonNumberFromPost(post),
-      episode_number: getEpisodeNumberFromPost(post),
-    });
-    return itemPromise.then((mediaItem) => {
-      const item = mediaItem;
-      item.postId = post.id;
-      return item;
-    });
-  }));
-}
+export default getMediaItem;
