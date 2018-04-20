e19cfc1045703f9db7ba2912b6b407cebb218b5e
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-04-20 18:55:30 +0100

    Simplify search action

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import theMovieDBAPI from '../apis/theMovieDBAPI';
 
 export const search = q => ({
@@ -6,17 +7,15 @@ export const search = q => ({
     .then(response => response.results)
     .then(data =>
       data.filter(item => item.media_type !== 'person').map((mediaItem) => {
-        const temp = {};
-        temp.id = mediaItem.id;
-        if (mediaItem.title) { temp.title = mediaItem.title; }
-        if (mediaItem.name) { temp.title = mediaItem.name; }
-        temp.img = mediaItem.poster_path;
-        temp.media_type = mediaItem.media_type;
-        if (mediaItem.media_type === 'tv') { temp.media_type = 'show'; }
         const date = mediaItem.release_date ?
           mediaItem.release_date : mediaItem.first_air_date;
-        temp.year = date && new Date(date).getFullYear();
-        return temp;
+        return {
+          id: mediaItem.id,
+          title: _.get(mediaItem, 'title') || _.get(mediaItem, 'name') || 'No title',
+          img: mediaItem.poster_path,
+          media_type: mediaItem.media_type === 'tv' ? 'show' : 'movie',
+          year: date && new Date(date).getFullYear(),
+        };
       })) : new Promise(resolve => resolve([])),
   meta: {
     globalError: 'Sorry, search failed',
