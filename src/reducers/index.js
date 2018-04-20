f48a11506397c38febdf82581b87562394ee1d1f
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-04-20 02:27:19 +0100

    Get media items from state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import postReducer from './postReducer';
-import movieReducer from './mediaReducer';
+import movieReducer, * as fromMedia from './mediaReducer';
+import searchReducer from './searchReducer';
 
 export default combineReducers({
-  posts: postReducer,
   media: movieReducer,
   router: routerReducer,
+  search: searchReducer,
 });
+
+export const getMediaItem = (state, mediaType, path) =>
+  fromMedia.getMediaItem(state.media, mediaType, path);
+
+export const getMediaItemState = (state, mediaType, path) =>
+  fromMedia.getMediaItemState(state.media, mediaType, path);
