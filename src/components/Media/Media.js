c60a36cc61403ae3cd8e8f7240996c7554335c5f
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-04-20 02:27:46 +0100

    Media for show and movie page for now

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,67 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Dotdotdot from 'react-dotdotdot';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './Media.less';
-import noImageFound from '../../images/no-image-found.jpg';
-import PostsContainer from '../../containers/PostsContainer';
 
-// const loadingIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
-
-
-class Media extends React.Component {
-  static propTypes = {
-    mediaItem: PropTypes.shape().isRequired,
-  };
-  state = {
-    posterLoaded: false,
-  }
-  handlePosterLoaded = () => {
-    this.setState({
-      posterLoaded: true,
-    });
-  }
-  render() {
-    const {
-      mediaItem,
-    } = this.props;
-    const { posterLoaded } = this.state;
-    const episodeInfo = mediaItem.mediaType === 'episode' && mediaItem && mediaItem[`season/${mediaItem.seasonNum}/episode/${mediaItem.episodeNum}`];
-    const backdropPath = mediaItem.backdrop_path && `https://image.tmdb.org/t/p/original${mediaItem.backdrop_path}`;
-    const title = (episodeInfo.name && episodeInfo.season_number && episodeInfo.episode_number && (`${episodeInfo.name} S${episodeInfo.season_number} E${episodeInfo.episode_number}`)) || (mediaItem.title && mediaItem.title) || (mediaItem.name && mediaItem.name);
-    const overview = (episodeInfo.overview && episodeInfo.overview) || (!episodeInfo && mediaItem.overview && mediaItem.overview) || "Sorry, we don't have an overview for this item";
-    const poster = (mediaItem.poster_path && `https://image.tmdb.org/t/p/w200${mediaItem.poster_path}`) || noImageFound
-    || (mediaItem.overview && mediaItem.overview);
-    const category = `tmdbid-${mediaItem.id}`;
-    return (
-      <div id={mediaItem.id} className="MediaItem">
-        <div
-          className="MediaItem__background"
-          style={{ background: `linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url(${backdropPath})` }}
-        >
-          <div className="MediaHeader">
-            <div className="MediaHeader__poster"><img onLoad={this.handlePosterLoaded} alt="poster" src={poster} /></div>
-            {posterLoaded &&
-            <div className="MediaHeader__info">
-              <div className="MediaHeader__info__title">
-                {title}
-              </div>
-              <div className="MediaHeader__info__overview">{overview}</div>
-              {episodeInfo.season_number && episodeInfo.episode_number && (
-                <div className="MediaHeader__link">
-                  <Link to={`/show/${mediaItem.id}`}>Go to show</Link>
-                </div>
-              )}
-            </div>
-          }
-          </div>
+const Movie = props => (
+  <div className="MediaItem">
+    <div
+      className="MediaItem__background"
+      style={{ background: `linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url(${props.backdropPath})` }}
+    >
+      <div className="MediaHeader">
+        <div className="MediaHeader__poster">
+          <img alt="poster" src={props.poster} style={{ postition: 'absolute' }} />
         </div>
-        <div style={{ padding: 24 }}>
-          <PostsContainer category={category} />
+        <div className="MediaHeader__info">
+          <div className="MediaHeader__info__title">
+            {props.title}
+          </div>
+          <div className="MediaHeader__info__overview">{props.overview}</div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
-export default Media;
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  overview: PropTypes.string.isRequired,
+  backdropPath: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+export default withRouter(Movie);
