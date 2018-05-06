db9c8e43c26fa84eb73c878a3a331d94553a2073
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-05-06 17:12:08 +0100

    Console log promise rejections in development

--- src/errorMiddleware.js
+++ src/errorMiddleware.js
@@ -2,9 +2,12 @@ import { message } from 'antd';
 
 export default () => next => (action) => {
   if (action.payload !== null && typeof action.payload === 'object' && typeof action.payload.then === 'function' && action.meta.globalError) {
-    return next(action).catch(() => {
+    return next(action).catch((err) => {
       // show antd message with global error in action meta
       message.error(action.meta.globalError);
+      if (process.env.NODE_ENV === 'development') {
+        console.log(err); /* eslint-disable-line no-console */
+      }
     });
   }
   return next(action);
