f1ceeea6a576cfaa5ad763e52126c0e1aa22a185
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-05-07 01:41:20 +0100

    Same image size as mobile landscape

--- src/helpers/mediaHelpers.js
+++ src/helpers/mediaHelpers.js
@@ -4,21 +4,21 @@ import noImageFound from '../images/no-image-found.jpg';
 const movieDetails = movie => ({
   title: (movie.title && movie.year && `${movie.title} (${movie.year})`) || movie.title || 'No title',
   overview: movie.overview || 'No overview',
-  backdropPath: (movie.backdrop_path && `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`) || '',
+  backdropPath: (movie.backdrop_path && `https://image.tmdb.org/t/p/w780${movie.backdrop_path}`) || '',
   posterPath: (movie.poster_path && `https://image.tmdb.org/t/p/w780${movie.poster_path}`) || noImageFound,
 });
 
 const showDetails = show => ({
   title: (show.title && show.year && `${show.title} (${show.year})`) || show.title || 'No title',
   overview: show.overview || 'No overview',
-  backdropPath: (show.backdrop_path && `https://image.tmdb.org/t/p/w1280${show.backdrop_path}`) || '',
+  backdropPath: (show.backdrop_path && `https://image.tmdb.org/t/p/w780${show.backdrop_path}`) || '',
   posterPath: (show.poster_path && `https://image.tmdb.org/t/p/w780${show.poster_path}`) || noImageFound,
 });
 
 export const seasonDetails = (show, seasonNum) => ({
   title: (seasonNum !== '0' && _.get(show, `seasons[${seasonNum}].air_date`) && `Season ${seasonNum} (${new Date(_.get(show, `seasons[${seasonNum}].air_date`)).getFullYear()})`) || `Season ${seasonNum}`,
   overview: _.get(show, `seasons[${seasonNum}].overview`) || 'No overview',
-  backdropPath: (show.backdrop_path && `https://image.tmdb.org/t/p/w1280${show.backdrop_path}`) || '',
+  backdropPath: (show.backdrop_path && `https://image.tmdb.org/t/p/w780${show.backdrop_path}`) || '',
   posterPath: (!_.isEmpty(_.get(show, `seasons[${seasonNum}].poster_path`)) && `https://image.tmdb.org/t/p/w780${show.seasons[seasonNum].poster_path}`) || showDetails(show).posterPath,
 });
 
@@ -27,7 +27,7 @@ export const EpisodeTitle = props => `${props.name} S${props.seasonNum} E${props
 export const episodeDetails = (show, seasonNum, episodeNum) => ({
   title: `${_.get(show, `seasons[${seasonNum}].episodes[${episodeNum}].name`)} S${seasonNum} E${episodeNum}`,
   overview: `${_.get(show, `seasons[${seasonNum}].episodes[${episodeNum}].overview`)}` || 'No overview',
-  backdropPath: (show.backdrop_path && `https://image.tmdb.org/t/p/w1280${show.backdrop_path}`) || '',
+  backdropPath: (show.backdrop_path && `https://image.tmdb.org/t/p/w780${show.backdrop_path}`) || '',
   posterPath: (_.get(show, `seasons[${seasonNum}].episodes[${episodeNum}].still_path`) && `https://image.tmdb.org/t/p/w780${_.get(show, `seasons[${seasonNum}].episodes[${episodeNum}].still_path`)}`) || (show.backdrop_path && `https://image.tmdb.org/t/p/w780${show.backdrop_path}`) || noImageFound,
 });
 
