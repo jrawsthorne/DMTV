78412b51720146216319f98b5da835aa30a42fb7
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-05-25 19:06:47 +0100

    Fetch similar actions

--- src/actions/mediaActions.js
+++ src/actions/mediaActions.js
@@ -10,6 +10,7 @@ import {
   USER_RATE_CHANGE,
   SUBSCRIBE_CHANGE,
   FETCH_SIMILAR_MOVIES,
+  FETCH_SIMILAR_SHOWS,
 } from './types';
 
 // fetch movie details and return object containing id, poster, backdrop, title, overview and year
@@ -40,6 +41,16 @@ export const fetchSimilarMovies = id => ({
     globalError: 'Sorry, there was an error fetching similar movies',
     id,
     type: 'movies',
+  },
+});
+
+export const fetchSimilarShows = id => ({
+  type: FETCH_SIMILAR_SHOWS,
+  payload: theMovieDBAPI.tvSimilar(id).then(res => _.shuffle(res.results).slice(0, 5)),
+  meta: {
+    globalError: 'Sorry, there was an error fetching similar shows',
+    id,
+    type: 'shows',
   },
 });
 
