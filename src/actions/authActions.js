5cc42e9273886e34abbc8ed0a7a097505e4be8b7
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-06-08 21:04:59 +0100

    Only add auth header for api calls that need it

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -7,8 +7,8 @@ export const login = () => ({
   type: LOGIN,
   /* server side login */
   payload: axios.post(`${process.env.API_URL}/users/login`, { accessToken: Cookie.get('access_token') }).then((res) => {
-    /* all future api requests send the authorization token */
-    axios.defaults.headers.common.Authorization = `Bearer ${res.data.token}`;
+    /* add app token to cookies */
+    Cookie.set('token', res.data.token);
     return res.data;
   }).catch(() => {
     /* remove the tokens if there's an error logging in */
@@ -34,14 +34,16 @@ export const logout = () => (dispatch) => {
 /* fetch all the ratings for the current user */
 export const fetchUserRatings = () => ({
   type: FETCH_USER_RATINGS,
-  payload: axios.get(`${process.env.API_URL}/users/ratings`).then(res => res.data),
+  /* add authorization header */
+  payload: axios.get(`${process.env.API_URL}/users/ratings`, { headers: { Authorization: `Bearer ${Cookie.get('token')}` } }).then(res => res.data),
   meta: { globalError: 'Error fetching ratings' },
 });
 
 /* fetch all the shows and movies the user has subscribed to */
 export const fetchUserSubscriptions = () => ({
   type: FETCH_USER_SUBSCRIPTIONS,
-  payload: axios.get(`${process.env.API_URL}/users/subscriptions`).then(res => res.data),
+  /* add authorization header */
+  payload: axios.get(`${process.env.API_URL}/users/subscriptions`, { headers: { Authorization: `Bearer ${Cookie.get('token')}` } }).then(res => res.data),
   meta: { globalError: 'Error fetching subscriptions' },
 });
 
