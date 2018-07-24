cd121bc0cf73881c1a8b500013e4edf97ba3e6f5
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-07-24 22:41:52 +0100

    Reply button

--- src/components/post/Buttons/ReplyButton.js
+++ src/components/post/Buttons/ReplyButton.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Button } from '../../../styles/theme';
+
+const StyledReplyButton = Button.extend`
+  margin-left: 10px;
+`;
 
 const ReplyButton = ({
   handleShowClick, showReplyBox,
 }) => (
-  <Button onClick={handleShowClick} style={{ marginLeft: 10 }}>{showReplyBox ? 'Close' : 'Reply'}</Button>
+  <StyledReplyButton size="small" type={showReplyBox ? 'info' : 'default'} onClick={handleShowClick}>{showReplyBox ? 'Close' : 'Reply'}</StyledReplyButton>
 );
 
 ReplyButton.propTypes = {
