a37cbd3c2b21a693478c95fd5eac619ae227f9f1
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-07-24 22:42:04 +0100

    Show replies button

--- src/components/post/Buttons/ShowRepliesButton.js
+++ src/components/post/Buttons/ShowRepliesButton.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Button } from '../../../styles/theme';
 
 const ShowRepliesButton = ({
-  handleShowRepliesClick, count,
+  handleShowRepliesClick, count, className,
 }) => (
-  <Button style={{ marginLeft: 10 }} onClick={handleShowRepliesClick}>View {count} {count === 1 ? 'reply' : 'replies'}</Button>
+  <Button className={className} margin="0 0 0 10px" size="small" onClick={handleShowRepliesClick}>View {count} {count === 1 ? 'reply' : 'replies'}</Button>
 );
 
 ShowRepliesButton.propTypes = {
   handleShowRepliesClick: PropTypes.func.isRequired,
   count: PropTypes.number.isRequired,
+  className: PropTypes.string,
+};
+
+ShowRepliesButton.defaultProps = {
+  className: null,
 };
 
 export default ShowRepliesButton;
